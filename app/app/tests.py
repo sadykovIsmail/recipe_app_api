0010729535e087880d806ebe61457f0ed39ca693
Ismail <ghvh8176@gmail.com>
2026-01-31 14:57:22 -0600

    testing with django

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+    def test_add_number(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)

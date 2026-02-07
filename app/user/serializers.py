88da24db9e736ebdd51bbf63b0c94200979a5a37
Ismail <ghvh8176@gmail.com>
2026-02-07 13:58:13 -0600

    implement create user api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,16 +1,30 @@
-"""Serializers for the user API View."""
 from django.contrib.auth import get_user_model
-
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSeralizer):
-    """Serializer for the user object."""
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object"""
 
     class Meta:
         model = get_user_model()
-        fields = ['email', 'password', 'name']
-        extra_kwards = {'passwords': {'write_only': True, 'min_length': 5}}
+        fields = ('email', 'password', 'name')
+        extra_kwargs = {
+            # Password can be sent IN, but never sent OUT
+            'password': {
+                'write_only': True,
+                'min_length': 5
+            }
+        }
 
     def create(self, validated_data):
-        """Create and return a user with encrypted password."""
+        """
+        Create and return a user with encrypted password.
+
+        Express equivalent:
+        User.create({
+          email,
+          password: hash(password),
+          name
+        })
+        """
         return get_user_model().objects.create_user(**validated_data)

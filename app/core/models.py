b1abea6b72a69df550dbfa60b783d89bf9d1c7f2
Ismail <ghvh8176@gmail.com>
2026-02-08 10:53:32 -0600

    make models for recipe app

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@
 Database models.
 This file contains all database models for the project.
 """
-
+from django.conf import settings
 # Import Django's base model system
 from django.db import models
 
@@ -76,3 +76,18 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # Field used for authentication instead of username
     USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title

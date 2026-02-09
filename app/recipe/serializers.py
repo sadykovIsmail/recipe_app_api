659794c8ece1b406fb05a90859adfe2785add196
Ismail <ghvh8176@gmail.com>
2026-02-09 15:58:10 -0600

    implement ingredient listing

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -8,8 +8,16 @@ Serializers convert:
 from rest_framework import serializers
 
 # Import models used by serializers
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredienst"""
+
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 class TagSerializer(serializers.ModelSerializer):
     """

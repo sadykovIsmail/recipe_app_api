fb9b15262db93f188fb6008efb8bc8fda72301e4
Ismail <ghvh8176@gmail.com>
2026-02-12 12:17:00 -0600

    implement updating ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -111,10 +111,15 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validate_data):
         """"Update recipe."""
         tags = validate_data.pop('tags', None)
+        ingredients = validate_data.pop('ingredients', None)
 
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            instance.Ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validate_data.items():
             setattr(instance, attr, value)
